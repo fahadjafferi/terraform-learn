98f3e307b36501763559b5412494b23768c824d7
Fahad Jafferi <fahad.jafferi@gmail.com>
2024-03-31 21:18:25 +0200

    adding provisioners

--- main.tf
+++ main.tf
@@ -123,22 +123,22 @@ output "aws_ami_id" {
   
 # }
 
-# resource "aws_instance" "mypp-server" {
-#     ami = data.aws_ami.latest-amazon-linux-image.id
-#     instance_type = var.instance_type
-#     subnet_id = aws_subnet.myapp-subnet-1.id
-#     vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-#     availability_zone = var.avail_zone
-#     associate_public_ip_address = true
-#     key_name = aws_key_pair.ssh-key.key_name
+resource "aws_instance" "mypp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
 
-#     user_data = file("entry-script.sh")
-#     tags = {
-#          Name = "${var.env_prefix}-server"
-#     }
+    user_data = file("entry-script.sh")
+    tags = {
+         Name = "${var.env_prefix}-server"
+    }
 
 
-# }
+}
 
 resource "aws_key_pair" "ssh-key" {
     key_name = "server-key"
