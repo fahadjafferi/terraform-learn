692edd388d327c2c21355aa218d28b191a8a0b65
Fahad Jafferi <fahad.jafferi@gmail.com>
2024-03-31 18:35:56 +0200

    added shell script for docker

--- main.tf
+++ main.tf
@@ -13,9 +13,8 @@ variable env_prefix{}
 
 variable my_ip {}
 
-variable "instance_type" {
-  
-}
+variable instance_type {}
+variable public_key_location{}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -119,21 +118,35 @@ output "aws_ami_id" {
   
 }
 
+# output "ec2_public_ip" {
+#    value = aws_instance.mypp-server.public_ip
+  
+# }
 
-resource "aws_instance" "mypp-server" {
-    ami = data.aws_ami.latest-amazon-linux-image.id
-    instance_type = var.instance_type
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-    availability_zone = var.avail_zone
-    associate_public_ip_address = true
-    key_name = "server-key-pair"
+# resource "aws_instance" "mypp-server" {
+#     ami = data.aws_ami.latest-amazon-linux-image.id
+#     instance_type = var.instance_type
+#     subnet_id = aws_subnet.myapp-subnet-1.id
+#     vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+#     availability_zone = var.avail_zone
+#     associate_public_ip_address = true
+#     key_name = aws_key_pair.ssh-key.key_name
 
-    tags = {
-         Name = "${var.env_prefix}-server"
-    }
+#     user_data = file("entry-script.sh")
+#     tags = {
+#          Name = "${var.env_prefix}-server"
+#     }
 
+
+# }
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.public_key_location)
+  
 }
+
+
 
 
 # resource "aws_route_table_association" "main-rtb-subnet" {
