fde5cd03e7a95e95ca59e7d014143f566d8a1ee5
Fahad Jafferi <fahad.jafferi@gmail.com>
2024-03-31 22:49:02 +0200

    updated main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ variable my_ip {}
 variable instance_type {}
 variable public_key_location{}
 
+
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -123,27 +124,27 @@ output "aws_ami_id" {
   
 # }
 
-# resource "aws_instance" "mypp-server" {
-#     ami = data.aws_ami.latest-amazon-linux-image.id
-#     instance_type = var.instance_type
-#     subnet_id = aws_subnet.myapp-subnet-1.id
-#     vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-#     availability_zone = var.avail_zone
-#     associate_public_ip_address = true
-#     key_name = aws_key_pair.ssh-key.key_name
+resource "aws_instance" "mypp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
 
-#     user_data = file("entry-script.sh")
-#     tags = {
-#          Name = "${var.env_prefix}-server"
-#     }
+    user_data = file("entry-script.sh")
+    tags = {
+         Name = "${var.env_prefix}-server"
+    }
 
 
-# }
+}
 
 resource "aws_key_pair" "ssh-key" {
     key_name = "server-key"
     public_key = file(var.public_key_location)
-  
+
 }
 
 
